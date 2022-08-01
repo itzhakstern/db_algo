30ce6ba48e61b28ab23304a4e2559658db906370
itzhakshtern <itzhak7915@gmail.com>
2022-08-01 20:51:22 +0300

    all algo

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
+from itertools import combinations
 
 
 
-# F need be an array of tuples 
 def closure(X, F):
     V = X
     len_v = len(V)
@@ -14,15 +14,9 @@ def closure(X, F):
             return V
         len_v = len(V)
 
-# print(closure('c', f))
-# print(closure(r,f))
-
-
-def super_key(X, F, R):
+def is_super_key(X, F, R):
     V = closure(X, F)
     return sorted(V) == sorted(R) 
-
-# print(super_key('al', f,r))
 
 def minimize(X, F):
     for x in X:
@@ -30,13 +24,8 @@ def minimize(X, F):
             X = X.replace(x, '')
     return X
 
-# print(minimize('bc', f))
-
 def find_key(R, F):
     return minimize(R, F)
-
-
-# print(find_key(r, f))
 
 def all_keys(R, F):
     x = 0
@@ -45,7 +34,6 @@ def all_keys(R, F):
     keys = [K]
     while len(key_queue):
         x += 1
-        print(key_queue)
         K = key_queue.pop()
         for atr in F:
             y = ''.join(sorted(atr[1]))
@@ -58,29 +46,73 @@ def all_keys(R, F):
                     # s_min = minimize(s, F)
                     keys.append(s_min)
                     key_queue.append(s_min)
-    return set(keys)
-
-# print(all_keys(r, f))
+    return list(set(keys))
 
 def is_in_bcnf(R, F):
-    if len(R) == 2:
+    if len(R) <= 2:
         return True
-    pass
+    for atr in F:
+        if set(atr[1]).issubset(set(atr[0])) or is_super_key(atr[0], F, R):
+            continue
+        return False
+    return True
 
+def a_is_in_keys(a, keys):
+    for key in keys:
+        if a in key:
+            return True
+    return False
+
+def a_is_subset(a, atr_0):
+    return a in atr_0
 
 def is_in_3nf(R, F):
     if(is_in_bcnf(R, F)):
         return True
-    pass
-
+    keys = all_keys(R, F)
+    # flag = False
+    for atr in F:
+        if not is_super_key(atr[0], F, R):
+            return False
+        for a in atr[1]:
+            if (not a_is_in_keys(a, keys)) and (not a_is_subset(a,atr[0])):
+                return False
+    return True
 
 # אלגוריתם לחישוב שימור תלויות
-def id_dependency_preserving(R, sub_R, F):
-    pass
+def is_dependency_preserving(sub_R, F):
+    dependency = {}
+    for atr in F:
+        z = atr[0]
+        while True:
+            z_1 = z
+            for r in sub_R:
+                close_intersuction_z_ri = closure(''.join(set(z).intersection(set(r))), F)
+                intersuction_zri_ri = set(close_intersuction_z_ri).intersection(set(r))
+                z = intersuction_zri_ri.union(set(z))
+            if set(z_1) == z:
+                if(not set(atr[1]).issubset(z)):
+                    dependency[atr] = [''.join(z), False]
+                else:
+                    dependency[atr] = [''.join(z), True]
+                break
+    flug = True
+    for x in dependency:
+        if not dependency[x][1]:
+            flug = False
+    print("*****    ", flug, "     ******")
+        
+    return dependency
 
 # אלגוריתם לחישוב קבוצת התלויות F_R_i
-def compute_dependencies_in_projection(R, R_i, F):
-    pass
+def compute_dependencies_in_projection(R_i, F):
+    res = [''.join(l) for i in range(len(R_i)) for l in combinations(R_i, i+1)]
+    G = []
+    for r in res:
+        closure_r = closure(r, F)
+        intrsection_ri_r = set(closure_r).intersection(set(R_i))
+        G.append((r,''.join(intrsection_ri_r)))
+    return G
 
 # אלגוריתם למציאת כיסוי מינימלי
 def compute_minimal_cover(F):
@@ -107,17 +139,57 @@ def compute_minimal_cover(F):
         if flug:
             counter += 1
         len_G -= 1
-
-
     return G
 
 # אלגוריתם לפירוק 3NF
 def find_3NF_decomposition(R, F):
-    pass
+    G = compute_minimal_cover(F)
+    res = []
+    for atr in G:
+        res.append(atr[0] + atr[1])
+    keys = all_keys(R, F)
+    flag = False
+    for r in res:
+        if ''.join(sorted(r)) in keys:
+            flag = True
+            break
+    if not flag:
+        res.append(keys[0])
+    res2 = []
+    for i in range(len(res)):
+        res_1 = res[:i] + res[i+1:]
+        is_subset = False
+        for r in res_1:
+            if not set(res[i]).issubset(set(r)):
+                continue
+            is_subset = True
+        if is_subset:
+            continue
+        res2.append(res[i])
+    return res2
+    
+
+        
+
+        
+         
+
 
 # אלגוריתם לפירוק BCNF
 def find_BCNF_decomposition(R, F):
-    pass
+    if is_in_bcnf(R,F):
+        return R
+    violation = ''
+    for atr in F:
+        if (not set(atr[1]).issubset(set(atr[0]))) and (not is_super_key(atr[0], F, R)):
+            violation = atr
+            break
+    R_1 = closure(violation[0],F)
+    R_2 = ''.join(set(violation[0]).union(set(R).difference(R_1)))
+    return find_BCNF_decomposition(R_1,compute_dependencies_in_projection(R_1,F)), (find_BCNF_decomposition(R_2,compute_dependencies_in_projection(R_2,F)))
+
+
+     
 
 # F = [('a','b'),('abcd','e'),('ej','gh'),('acdj','eg')]
 
@@ -127,5 +199,30 @@ f2 = [('eg', 'ab'), ('gc', 'ae'),('ec', 'db'),('ab', 'cdg'), ('e', 'ag')]
 f3 = [('bc', 'ade'), ('db', 'ace'),('c', 'ad'),('d', 'ce')]
 r2 = "abcde"
 str = ""
-print(all_keys(r2,f3))
-print(compute_minimal_cover(f3))
+sub_r1 = ['ab', 'bc', 'cd']
+f4 = [('a', 'b'), ('b', 'c'),('c', 'd'),('d', 'a')]
+sub_r2 = ['abde', 'bec']
+f5 = [('a', 'abcde'), ('bc', 'a'),('de', 'c')]
+sub_r3 = ['abc', 'ad']
+f6 = [('a', 'bc'), ('b', 'ad'),('c', 'd')]
+sub_r4 = ['aeg', 'bce', 'cdeg']
+f7 = [('bc', 'g'), ('ce', 'ad'),('eg', 'abd'),('ae', 'b'), ('cg', 'ae')]
+
+r3 = 'abc'
+r4 = 'ad'
+f8 = [('ab', 'c'),('d', 'a')]
+
+r5 = 'abce'
+r6 = 'abcdeg'
+f9 = [('eg', 'ab'), ('gc', 'ae'), ('ec', 'bd'), ('ab', 'cdg'), ('e','ag')]
+
+# print(all_keys(r2,f3))
+# print(compute_minimal_cover(f3))
+# print(is_dependency_preserving(sub_r4, f7))
+# ff = compute_dependencies_in_projection(r6, f9)
+# print(ff)
+# print(all_keys(r6, f9))
+# print(is_in_bcnf(r6, ff))
+# print(is_in_3nf(r6, ff))
+print(find_BCNF_decomposition(r6,f9))
+
